d6140893892b13347d11236b1e0fa09adcc84f4b
Felipe Dantas <dantas.contatod@gmail.com>
2024-07-18 18:27:07 -0300

    testing types at api callls

--- src/app/services/listUsers.ts
+++ src/app/services/listUsers.ts
@@ -5,6 +5,12 @@ export async function listUsers() {
   await sleep();
   const response = await fetch("http://localhost:3000/users");
   const body = await response.json();
-
   return body as IUser[];
 }
+
+//Using Generics
+// export async function listUsers():Promise<IUser[]> => {
+//   await sleep();
+//   const response = await fetch("http://localhost:3000/users");
+//   return await response.json();
+// }
